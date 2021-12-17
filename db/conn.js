106073eb08dcc3934b99114f13e697d024c2d388
Aditya GCloud <adityag.ug19.cs@nitp.ac.in>
2021-12-17 04:45:22 +0000

    Fix connection to database

--- db/conn.js
+++ db/conn.js
@@ -1,23 +1,10 @@
-const { MongoClient } = require('mongodb');
-const uri = process.env.DATABASE;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connecton success");
-  // perform actions on the collection object
-    client.close();
-});
-
-/* const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const DB = process.env.DATABASE;
 mongoose.connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
 }).then(() => {
     
     console.log(`connection successful`);
-}).catch((err) => console.log(`connection error`));
-*/
+}).catch((err) => console.log(`connection error`,err));
